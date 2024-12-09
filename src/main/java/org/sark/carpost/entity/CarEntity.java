ade487e95c6591ff313489e065848efd21ea22d5
madao-madao <mamamatao@yandex.ru>
2024-12-10 00:37:06 +0300

    feat(database): Заполнение базы данных

--- src/main/java/org/sark/carpost/entity/CarEntity.java
+++ src/main/java/org/sark/carpost/entity/CarEntity.java
@@ -2,7 +2,6 @@ package org.sark.carpost.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
